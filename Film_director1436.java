115d8e35b97c80cdb42e351659dca4d758825936
jsm512 <wlstmdals12@naver.com>
2022-03-22 16:41:20 +0900

    Create Film_director1436.java

---
+++ Film_director1436.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Film_director1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int number = 666;
+        int cnt = 1;
+
+        while(cnt != n){
+            number++;
+            if(String.valueOf(number).contains("666")){
+                cnt++;
+            }
+        }
+        System.out.println(number);
+    }
+}
