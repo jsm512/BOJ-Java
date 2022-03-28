af56bd45c5901204e7ae62ded83979d33e17752b
jsm512 <wlstmdals12@naver.com>
2022-03-28 20:30:26 +0900

    Create four_number10824.java

---
+++ four_number10824.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class four_number10824 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] number = br.readLine().split(" ");
+        Long a = Long.parseLong(number[0] + number[1]);
+        Long b = Long.parseLong(number[2] + number[3]);
+        System.out.println(a + b);
+    }
+}
